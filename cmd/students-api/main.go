e179db59eac7f7730bfd46566f0d1fc86d257fc5
Rishav83523 <rishav.raj@calsoftinc.com>
2025-12-27 11:41:14 +0530

    refactor: enhance configuration handling and server setup

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mr-raj2001/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to student api")
+	//load config
+
+	cfg := config.MustLoad()  //calling the MustLoad function from config package to get the configuration
+	//databse setup
+	//setup router
+	//net http package for setting up http server
+	router := http.NewServeMux()  //creating a new HTTP request multiplexer
+
+	router.HandleFunc("GET /",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})//handler function with response writer and request pointer
+    
+
+	//setup http server
+
+	server := http.Server {
+		Addr : cfg.Addr,        //setting the address from the config
+		Handler: router,        //setting the handler to the router we created
+	}
+
+	fmt.Printf("Server started %s\n", cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
+
+	
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,45 +9,45 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr  string `yaml:"address" env-required:"true"`
 }
 
 //env-default:"production" means if env variable is not set then default to production
 
 type Config struct {
 	Env         string `yaml:"env" env:"ENV" env-required:"true" ` //basically we are saying that in the yaml file the field name is env but in struct it is Env and annotations for env variable
-	storagePath string `yaml:"storage_path" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 //Mustload dont return error , just do fatal close the application
 func MustLoad() *Config {
 	var configPath string
 
-	configPath = os.Getenv("CONFIG_PATH")
+	configPath = os.Getenv("CONFIG_PATH")  // basically we are checking if config path is set in env variable for the command we write in terminal
 
 	if configPath == "" {
 		flags := flag.String("config", "", "path to the configuration file")
-		flag.Parse()
+		flag.Parse()  //parsing the command line arguments
 
-		configPath = *flags
+		configPath = *flags  //dereferencing the pointer to get the actual value
 
 		if configPath == "" {
-			log.Fatal("config path must be provided")
+			log.Fatal("config path must be provided")  //if config path is still empty then we log fatal and exit
 		}
 	}
 
 
 	if _,err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config file does not exist at path: %s",configPath)
+		log.Fatalf("config file does not exist at path: %s",configPath)  //checking if the file exists at the given path
 	}
 
-	var cfg Config 
+	var cfg Config   //creating an instance of config struct
 
 	err := cleanenv.ReadConfig(configPath, &cfg)
 	if err != nil {
-		log.Fatalf("failed to read config: %s", err.Error())
+		log.Fatalf("failed to read config: %s", err.Error()) //reading the config file and populating the struct
 	}
 
 
-	return &cfg
+	return &cfg  //returning the pointer to the config struct
 }

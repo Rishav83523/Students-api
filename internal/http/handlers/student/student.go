afd201cfb077d348f86b5a4c41ae67077b703077
Rishav83523 <rishav.raj@calsoftinc.com>
2025-12-29 11:02:34 +0530

    refactor: enhance error handling and response structure in student creation

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -3,6 +3,7 @@ package student
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
@@ -17,14 +18,22 @@ func New() http.HandlerFunc {
         var student types.Student  //creating an instance of student struct from types package
 
 		err := json.NewDecoder(r.Body).Decode(&student)  //r.io Reader is passed to NewDecoder to read the request body  //Body type is io.ReadCloser which implements Reader interface
-        
+        	slog.Info("creating a student")
 		// error has Is to check the type of errr 
 		if errors.Is(err, io.EOF) {
-			response.Writejson(w, http.StatusBadRequest, err.Error())
+			response.Writejson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body"))) //if request body is empty we get EOF error so we send bad request response
 			return 
 		}
 
-		slog.Info("creating a student")
+		if err != nil {
+			response.Writejson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("invalid json body: %w", err))) //if there is any other error in decoding we send bad request response
+			return
+		}
+
+		//validate request by using request validator package playground
+		
+
+	
 		//in go we have to serialize the data in request body to struct
 		response.Writejson(w, http.StatusCreated, map[string]string{"success": "student created successfully"})  //sending response in json format with status code 201 using response package made by us
 	}
--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Status string `json:"status"`
+	Error string `json:"error"`  //basically we are saying that in json response the field name is error but in struct it is Errorit is seralizing using package encoding json
+}
+
+const (
+	StausOk = "ok"
+	StatusError = "error"
+)
+
 //to send response in json format
 //data `any` it mans we dont know the type of data it can be struct , map , slice etc which we are going to write
 func Writejson(w http.ResponseWriter, statusCode int, data any) error {
@@ -12,4 +22,12 @@ func Writejson(w http.ResponseWriter, statusCode int, data any) error {
 	w.WriteHeader(statusCode) 
 	
 	return json.NewEncoder(w).Encode(data)//encoding the data to json and writing it to response writer it return error type 
+}
+
+
+func GeneralError( err error) Response {
+     return Response{ 
+		Status: StatusError,
+		Error: err.Error(),
+	 }
 }
